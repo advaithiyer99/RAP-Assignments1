2a86300289643ebc18e10fabd080ba40a8293979
unknown <advaithiyer99@yahoo.com>
2021-06-04 17:29:39 -0700

    initial commit

---
+++ day-8/src/examples/StringMethods.java
@@ -0,0 +1,33 @@
+package examples;
+
+public class StringMethods {
+	public static void main(String[] args) {
+		String myString = "Hello there, my name is Advaith.";
+		
+		// charAt - returns a char
+		System.out.println(myString.charAt(0));
+		char myChar = myString.charAt(0);
+		String myNewString = String.valueOf(myChar);
+		System.out.println(myNewString instanceof String);
+		
+		// indexOf - returns an int
+		System.out.println(myString.indexOf('e'));
+		System.out.println(myString.lastIndexOf('e'));
+		
+		// length - returns an int
+		System.out.println(myString.length());
+		
+		// substring - returns a string
+		System.out.println(myString.substring(5, 25));
+		System.out.println(myString);
+		
+		// contains - returns a boolean
+		System.out.println(myString.contains("e"));
+		System.out.println(myString.contains(Character.toString('e')));
+		
+		for (char letter : myString.toCharArray()) {
+			System.out.println(letter);
+		}
+		
+	}
+}
